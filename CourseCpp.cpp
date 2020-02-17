108b4f1e07e2dbfd4e79716db9e299c1d54d39b1
yanvar <yanvar@gmail.com>
2020-02-18 00:14:25 +0200

    Add return

--- CourseCpp.cpp
+++ CourseCpp.cpp
@@ -3,9 +3,12 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!" << endl;
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
