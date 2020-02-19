b8ecdbe65837747412ab31cf1d178e69c2e0649d
Konstantin Stelmakh <kosta223@gmail.com>
2020-02-20 00:22:52 +0200

    Battery - updated + no compilation errors

--- Battery.h
+++ Battery.h
@@ -1,30 +1,29 @@
 #pragma once
+#include <iostream>
 
-#include <map> 
+#define D_BATTERY_MAX_CHARGE       (100) //TBD - max is needed to prevent overcharge!
+#define D_BATTERY_CONSUMPTION_RATE (1)
+#define D_BATTERY_ZERO_ENERGY      (0)
 
-#define D_BATTERY_MAX_CHARGE      (100) //TBD
-
-class Battery() {
+class Battery
+{
 public:
-	Battery(int chargeLevelInSteps);
+	Battery(int initChargeLevelInSteps);
 
 	// battery measured in "steps" units
-	void setBatteryLevelInSteps(int stepsToSet);
+	int setBatteryLevelInSteps(int stepsToSet);
 	int getBatteryLevelInSteps();
 	bool isBatteryEmpty();
 	int decrementBatterySingleStep();
-	int chargeBatterySingleStep();
+	int chargeBatteryDringSingleStep(); // only full steps are count (int)
 
 
 private:
-	int m_batteryLevelInSteps;
+	// all poewr measurements are normalized to single step unit
+	int batteryMaxCapacityInSteps;
+	int m_initBatteryLevelInSteps;  // asume it is the MAX power capacity allowed
+	int m_currentBatteryLevelInSteps;
 
-	
-
-	//battery is measured in "steps" units
-	int m_batteryFullCapacity;
 	int m_batteryConsumptionRate;
 	float m_batteryRechargeRate;
-
-
-}
+};
