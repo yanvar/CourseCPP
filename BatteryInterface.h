830b6551005d2343a600c4eef8b9044aacf71525
yanvar <yanvar@gmail.com>
2020-02-21 18:36:40 +0200

    Added interface functions

--- BatteryInterface.h
+++ BatteryInterface.h
@@ -5,5 +5,9 @@ class BatteryInterface
 {
 public:
 	virtual int stepsLeft() const = 0;
+	virtual bool isBatteryEmpty() const = 0;
+	virtual uint32_t setBatteryLevelInSteps(int stepsToSet) = 0;
+	virtual uint32_t decrementBatterySingleStep() = 0;
+	virtual uint32_t chargeBatteryDuringSingleStep() = 0;
 };
 
