e31f255456426017f13f42959207e6f291c73ed6
michalh <honig.michal@gmail.com>
2020-02-26 19:38:16 +0200

    Add api to cell

--- Robotalgo/CellInfo.cpp
+++ Robotalgo/CellInfo.cpp
@@ -1,1 +1,20 @@
 #include "CellInfo.h"
+
+using namespace robotalgo;
+
+CellInfo::CellInfo(bool isClean, bool stepToDocking, bool isWall)
+{
+	m_isClean = isClean;
+	m_stepsToDocking = stepToDocking;
+	m_isWall = isWall;
+}
+
+void CellInfo::visitCell()
+{
+	m_visited = true;
+}
+
+bool CellInfo::isVistedCell() const
+{
+	return m_visited;
+}
--- Robotalgo/CellInfo.h
+++ Robotalgo/CellInfo.h
@@ -5,11 +5,18 @@ namespace robotalgo
 {
 	class CellInfo
 	{
+	public:
+		CellInfo(bool isClean, bool stepToDocking, bool isWall);
+		void visitCell();
+		bool isVistedCell() const;
+
+	private:
 		bool m_isClean;
-		bool m_stepToDocking;
-		bool m_visted;
+		bool m_stepsToDocking;
+		bool m_visited = false;
 		bool m_isWall;
 		class CellInfo* m_cellneighbours[common::Neighbours_INDEX::NEIGHBOURS_TOTAL] = {nullptr};
 		bool m_isWallNeighbours[common::Neighbours_INDEX::NEIGHBOURS_TOTAL] = {};
+
 	};
 };
